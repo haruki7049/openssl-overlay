20cbe44c57a1ae04dd7ff9a0838ac0abd2fcb535
haruki7049 <tontonkirikiri@gmail.com>
2024-05-13 19:50:19 +0900

    chore: Init default.nix

---
+++ default.nix
@@ -0,0 +1,22 @@
+self: super: {
+  openssl = {
+    "1.0.2" = super.stdenv.mkDerivation {
+      pname = "openssl";
+      version = "1.0.2";
+
+      src = super.fetchFromGitHub {
+        owner = "openssl";
+        repo = "openssl";
+        rev = "OpenSSL_1_0_2";
+        sha256 = "sha256-so3qv87ph1mA3LMi4zOQ2hOlo9DApbuQc33GSWnBl/E=";
+      };
+
+      buildInputs = with super.pkgs; [ perl ];
+
+      buildPhase = ''
+        ./config --prefix=$out
+        make
+      '';
+    };
+  };
+}
